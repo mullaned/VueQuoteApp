087ef488a43e426871815d95af1037ed8e083848
David M <mullaned@gmail.com>
2018-09-24 17:11:08 +0100

    setting up props and slots

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="container">
-    
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
-        
+        data: function() {
+            return{
+                maxQuotes:10,
+                quotes: ['Just a Quote to see something'],
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
     }
 </script>
 
