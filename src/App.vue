112778b023a09a97723d1bad2721c37a599e3da6
David M <mullaned@gmail.com>
2018-09-24 17:55:57 +0100

    keep track of quotes and implement limit warning

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -13,6 +14,7 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
     export default {
         data: function() {
@@ -23,6 +25,9 @@
         },
         methods: {
             newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete a Quote first');
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(index){
@@ -31,7 +36,8 @@
         },
         components: {
             appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote
+            appNewQuote: NewQuote,
+            appHeader: Header
         }
     }
 </script>
